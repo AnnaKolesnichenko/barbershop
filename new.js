2826fa3241badac66716e87169f011a046e89699
AnnaKolesnichenko <a.040816.a@gmail.com>
2023-03-12 21:30:29 +0300

    Update new.js

--- new.js
+++ new.js
@@ -144,7 +144,7 @@ if(question == 'ECMAscript') {
 }*/
 
 
-const hours = 14;
+/*const hours = 14;
 const minutes = 26;
 let timestring;
 
@@ -174,3 +174,185 @@ if(a > 100 && b > 100) {
 } else {
   console.log(b + 512);
 }
+
+
+let link = 'https://my-site.com/about';
+// Пиши код нижче за цей рядок
+if(!link.endsWith('/')){
+  link = link + '/';
+}
+
+// Пиши код вище за цей рядок
+console.log(link);
+
+
+let hour = prompt("What hour is now?", '');
+if(hours < 17) {
+  console.log('Pending');
+}else if (hour >= 17 || hour <= 24) {
+  console.log('Expires');
+}else {
+  console.log('Overdue');
+}
+
+const daysUntilDeadline = 5;
+
+if (daysUntilDeadline === 0){
+  message = "Today";
+}else if(daysUntilDeadline === 1){
+  message="Tomorrow";
+}else if (daysUntilDeadline === 2){
+  message = 'Day after tomorrow';
+}else {
+  message = "Date in the future";
+}
+console.log(message);
+
+switch(daysUntilDeadline) {
+  case 0:
+    console.log("Today");
+    break;
+  case 1:
+    console.log("Tomorrow");
+    break;
+  case 2:
+    console.log("Day after tomorrow");
+    break;
+  default:
+    "Date in the future";
+    break;
+}
+
+
+const minimum = 20;
+const maximum = 100;
+for(let i = minimum; i <= maximum; i++) {
+  if(i%5 == 0) {
+    console.log(i);
+  } else {
+    continue;
+  }
+}
+
+const login = prompt("Enter your login please", '');
+let password;
+if (login === 'Admin') {
+
+  const password = prompt('Enter your password?');
+  if(password === 'I am admin') {
+    console.log("welcome")
+  }else {
+    console.log('Wrong password');
+  }
+} else if (login === '' || login === undefined) {
+  console.log('Cancelled');
+} else {
+  console.log("I do not recognize you");
+}*/
+
+const genres = ['Jazz', 'Blues'];
+genres.push('rock-n-roll');
+console.log(genres);
+console.log(genres[0]);
+console.log(genres[genres.length-1]);
+genres.pop(-1);
+console.log(genres);
+genres.unshift('Trance', 'Electro');
+console.log(genres);
+
+const values = '8 11';
+const nums = values.split(" ");
+console.log(nums);
+const square = nums[0] * nums[1];
+console.log(square);
+
+const namesA = 'Jacob,William,Solomon,Artemis';
+const phonesA = '89001234567,89001112233,890055566377,890055566300';
+
+const namesArr = namesA.split(" ");
+const phonesArr = phonesA.split(" ");
+
+
+const string = "Welcome to the future";
+const newStrgin = string.split(" ");
+console.log(newStrgin);
+const anyStr = newStrgin.slice(1, newStrgin.length-1);
+console.log(anyStr.join(" "));
+
+
+
+/*Example 6 - Массивы и строки
+Напиши скрипт который «разворачивает» строку (обратный порядок букв) и выводит ее в консоль.*/
+const stringFly = 'Welcome to the future';
+console.log(stringFly.split("").reverse().join(""));
+
+
+/*Example 7 - Сортировка массива с циклом
+Напиши скрипт сортировки массива строк в алфавитном порядке по первой букве элемента.*/
+const langs = ['python', 'javascript', 'c++', 'haskel', 'php', 'ruby'];
+const langsSort = langs.sort();
+console.log(langsSort);
+
+
+/*Example 8 - Поиск элемента
+Напиши скрипт поиска самого маленького числа в массиве. 
+Код должен работать для любого массива чисел. 
+Используй цикл для решения задачи.*/
+const numbers = [2, 17, 94, 1, 23, 37];
+let min;
+for(let i = 0; i <= numbers.length; i++){
+  min = 0;
+  if(numbers[i] < numbers[i-1]) {
+    min = numbers[i];
+    console.log(min);
+  }
+
+}
+
+//console.log(Math.min(2,17,94,1,23,37));
+//let min = numbers[0];
+//for(const num of numbers){
+  //if(num<min) {min=num}
+  const cart = [4,88,15,37];
+  const newCart = [33,77,88,99];
+  let total = 0;
+
+  const calculateTotalPrice = function(arr) {
+    for(const i of arr) {
+      total = total + i;
+    }
+    console.log("Total: ", total );
+    return total;
+  }
+
+  console.log(calculateTotalPrice(cart));
+  console.log(calculateTotalPrice(newCart));
+
+  
+  const logins = ['admin', 'any', 'qwerty'];
+  const findLogin = function(login, loginNeeded) {
+    if(login.includes(loginNeeded)) {
+      console.log("Succeed!")
+      return loginNeeded;
+    }else {
+      return "Wrong login, try again!";
+    }
+  }
+  console.log(findLogin(logins, 'admin'));
+  console.log(findLogin(logins, 'aoppple'));
+
+
+  const numberArr = [100,2,5,7,122,15];
+
+  const findSmallest = function (arr) {
+    let num = arr[0];
+    for(const i of arr) {
+      if(i<num) {
+        num=i;
+        return num;
+      }
+    }
+  }
+  console.log(findSmallest(numberArr));
+ 
+
