42afe869b92c633ff58610ff470379f169c17ac8
AnnaKolesnichenko <117039891+AnnaKolesnichenko@users.noreply.github.com>
2023-04-02 18:07:33 +0300

    Update new.js

--- new.js
+++ new.js
@@ -978,7 +978,33 @@ const atTheOldToad = {
   // Change code above this line
 };
 
-console.log("hello");
+
+const pizzaPalace = {
+  pizzas: ['Ultracheese', 'Smoked', 'Four meats'],
+  order(pizzaName, onSuccess, onError) {
+    if(this.pizzas.includes(pizzaName)) {
+      return onSuccess(pizzaName);
+    }
+    return onError(`There is no pizza with a name ${pizzaName} in the assortment.`);
+  },
+};
+// Change code above this line
+
+// Callback for onSuccess
+function makePizza(pizzaName) {
+  return `Your order is accepted. Cooking pizza ${pizzaName}.`;
+}
+
+// Callback for onError
+function onOrderError(error) {
+  return `Error! ${error}`;
+}
+
+// Method calls with callbacks
+console.log(pizzaPalace.order('Smoked', makePizza, onOrderError));
+console.log(pizzaPalace.order('Four meats', makePizza, onOrderError));
+console.log(pizzaPalace.order('Big Mike', makePizza, onOrderError));
+console.log(pizzaPalace.order('Vienna', makePizza, onOrderError));
 
 
 
